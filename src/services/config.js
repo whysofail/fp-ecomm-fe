a29fdf3771b04250af82805224bed312d3641f14
whysofail <52300127+whysofail@users.noreply.github.com>
2023-02-09 10:45:50 +0700

    Update config.js

--- src/services/config.js
+++ src/services/config.js
@@ -1,6 +1,6 @@
 // export const RootPath = import.meta.env.VITE_BASE_URL;
 // export const RootPath = 'https://flypass-api.up.railway.app'
-export const RootPath = 'https://fp-ecomm-be-production.up.railway.app/'
+export const RootPath = 'https://fp-ecomm-be-production.up.railway.app'
 
 // documentations
 // https://documenter.getpostman.com/view/23555317/2s8YswQBiG#43ac99c1-b38c-4d4d-b3c7-c32ccf038747
